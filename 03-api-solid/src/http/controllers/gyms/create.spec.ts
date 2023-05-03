f760b6adf2bac912cf49a633f6514066a94a8e9e
GustavoPaula <gustavopaula095@gmail.com>
2023-05-03 00:02:44 -0300

    feat: testes e2e de rotas de academias

---
+++ 03-api-solid/src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Create Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const response = await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'Some description.',
+        phone: '1199999999',
+        latitude: -22.6420889,
+        longitude: -47.202304,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
