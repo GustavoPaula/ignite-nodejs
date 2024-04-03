3e67e6a9f710eeda53bba07250daf9b2adafdad6
GustavoPaula <gustavopaula095@gmail.com>
2024-04-02 21:08:38 -0300

    feat: getters & setters das entidades

--- 04-clean-ddd/src/domain/entities/student.ts
+++ 04-clean-ddd/src/domain/entities/student.ts
@@ -7,6 +7,9 @@ interface StudentProps {
 }
 
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.props.name
+  }
 
   static create(props: StudentProps, id?: UniqueEntityID) {
     const student = new Student(props, id)
