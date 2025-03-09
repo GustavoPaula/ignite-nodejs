b70d83e53407d5554f86508dda7f080c3e3b584b
GustavoPaula <gustavosilvadepaula95@outlook.com>
2025-03-09 15:34:44 -0300

    feat: ignite nodejs

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,13 +1,43 @@
-import { PrismaClient } from "@prisma/client"
-import fastify from "fastify"
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from '@/env'
+import { usersRoutes } from '@/http/controllers/users/routes'
+import { gymsRoutes } from '@/http/controllers/gyms/routes'
+import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
 
-prisma.user.create({
-  data: {
-    name: "Gustavo de Paula",
-    email: "gustavo@email.com",
+app.register(fastifyCookie)
+
+app.register(usersRoutes)
+app.register(gymsRoutes)
+app.register(checkInsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
   }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
